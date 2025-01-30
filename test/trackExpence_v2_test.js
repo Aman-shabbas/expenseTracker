e5ad95dac5c5c52f59153c0544e21205df4edca6
Aman Shabbas <83913651+Aman-shabbas@users.noreply.github.com>
2025-01-30 14:04:36 +0530

    added test case for isEqualTo

--- test/trackExpence_v2_test.js
+++ test/trackExpence_v2_test.js
@@ -1,11 +1,38 @@
 import { describe, it } from "jsr:@std/testing/bdd";
-import { assertEquals, assert } from "jsr:@std/assert";
+import { assertEquals, assert, assertFalse } from "jsr:@std/assert";
 import { Expense } from "../src/trackExpenses_v2.js";
+
+describe("testing is Equal method", () => {
+  it("two instances of differance class should be false", () => {
+    const expenses1 = new Expense(1, 0);
+    const string = new String(2, 0);
+
+    assertFalse(expenses1.isEqualTo(string));
+  });
+
+  it("two instance of same class and different value should be false", () => {
+    const expense1 = new Expense(1, 0);
+    const expense2 = new Expense(2, 0);
+    const expense3 = new Expense(1, 2);
+
+    assertFalse(expense1.isEqualTo(expense2));
+    assertFalse(expense1.isEqualTo(expense3));
+    assertFalse(expense2.isEqualTo(expense3));
+  });
+
+  it("identical intances should be equal", () => {
+    const expense1 = new Expense(1, 40);
+    const expense2 = new Expense(1, 40);
+
+    assert(expense1.isEqualTo(expense2));
+  });
+});
 
 describe("testing add method", () => {
   it("adding two Expense should give rsulting Expense", () => {
     const expense1 = new Expense(2, 40);
     const expense2 = new Expense(2, 60);
+
     assert(expense1.add(expense2).isEqualTo(new Expense(5, 0)));
   });
 });
